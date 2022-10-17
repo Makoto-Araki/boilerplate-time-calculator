23f7f90b470f4b2e4d4f84441418f1158001906e
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-10-17 11:25:44 +0000

    メイン関数の修正

--- time_calculator.py
+++ time_calculator.py
@@ -1,12 +1,12 @@
 '''
-Time Calculation(MM)
+This function if for Time Calculation(MM)
 (ex)
   add_MM(45, 10) => [
-    55,  // Minutes after treatment
+    55,  // Minute after treatment
     0,   // Carry over for hours
   ]
   add_MM(45, 30) => [
-    15,  // Minutes after treatment
+    15,  // Minute after treatment
     1,   // Carry over for hours
   ]
 '''
@@ -47,76 +47,104 @@ def set_HH(hour, string):
     return hour
   else:
     return hour + 12
-  
+
 '''
-Time Calculation(HH)
-(ex: 11:00 AM)
-  add_HH(11, 'AM', 10) => [
-    9,  // Hour after treatment
-    0,  // Number of crossing the date
-    1,  // Number of switches between AM and PM
-  ]
-  add_HH(11, 'AM', 20) => [
-    7,  // Hour after treatment
-    1,  // Number of crossing the date
-    2,  // Number of switches between AM and PM
-  ]
-(ex: 11:00 PM)
-  add_HH(11, 'PM', 10) => [
-    9,  // Hour after treatment
-    1,  // Number of crossing the date
-    1,  // Number of switches between AM and PM
-  ]
-  add_HH(11, 'PM', 20) => [
-    7,  // Hour after treatment
-    1,  // Number of crossing the date
-    2,  // Number of switches between AM and PM
-  ]
+Return reverse notation
+(ex)
+  set_Notation('AM') => 'PM'
+  set_Notation('PM') => 'AM'
 '''
 
-def add_HH(hour, notation, duration):
-  
-  '''
-  Initialize return value
-  '''
-  
-  result = list()
-  
-  '''
-  Hour after treatment
-  '''
-  
-  HH = (hour + duration) % 12
-  
-  '''
-  Number of crossing the date
-  '''
-  
-  crossing = 0
-  for val in range(set_HH(hour, notation), set_HH(hour, notation) + duration):
-    if val % 24 == 0:
-      crossing += 1
-  
-  '''
-  Number of switches between AM and PM
-  '''
-  
-  switches = 0
-  for val in range(set_HH(hour, notation), set_HH(hour, notation) + duration):
-    if val % 12 == 0:
-      switches += 1
-  
-  '''
-  Return the return value
-  '''
-  
-  return [HH, crossing, switches]
-  
+def set_Notation(notation):
+  if notation == 'AM':
+    return 'PM'
+  if notation == 'PM':
+    return 'AM'
+
 '''
 Main function
+
 (ex)
-add_time('11:15 AM', '2:15') => '1:30 PM'
-add_time('11:15 PM', '2:15') => '1:30 AM'
+  add_time('11:00 AM', '0:05') => [
+   11,  // Hour after treatment
+    5,  // Minute after treatment
+    0,  // Number of crossing the date
+    0,  // Number of switches between AM and PM
+  ] => '11:05 AM'
+  
+(ex)
+  add_time('11:00 AM', '10:05') => [
+    9,  // Hour after treatment
+    5,  // Minute after treatment
+    0,  // Number of crossing the date
+    1,  // Number of switches between AM and PM
+  ] => '9:05 PM'
+  
+(ex)
+  add_time('11:00 AM', '20:05') => [
+    7,  // Hour after treatment
+    5,  // Minute after treatment
+    1,  // Number of crossing the date
+    2,  // Number of switches between AM and PM
+  ] => '7:05 AM (next day)'
+  
+(ex)
+  add_time('11:00 AM', '0:65') => [
+    0,  // Hour after treatment
+    5,  // Minute after treatment
+    0,  // Number of crossing the date
+    0,  // Number of switches between AM and PM
+  ] => '12:05 AM'
+
+(ex)
+  add_time('11:00 AM', '20:05', 'Monday') => [
+    7,  // Hour after treatment
+    5,  // Minute after treatment
+    1,  // Number of crossing the date
+    2,  // Number of switches between AM and PM
+    2,  // Sunday: 0, ... Saturday: 6
+  ] => '7:05 AM, Tuesday (next day)'
+
+(ex)
+  add_time('11:00 PM', '0:05') => [
+   11,  // Hour after treatment
+    5,  // Minute after treatment
+    0,  // Number of crossing the date
+    0,  // Number of switches between AM and PM
+  ] => '11:05 PM'
+  
+(ex)
+  add_time('11:00 PM', '10:05') => [
+    9,  // Hour after treatment
+    5,  // Minute after treatment
+    1,  // Number of crossing the date
+    1,  // Number of switches between AM and PM
+  ] => '9:05 AM (next day)'
+  
+(ex)
+  add_time('11:00 PM', '20:05') => [
+    7,  // Hour after treatment
+    5,  // Minute after treatment
+    1,  // Number of crossing the date
+    2,  // Number of switches between AM and PM
+  ] => '7:05 PM (next day)'
+  
+(ex)
+  add_time('11:00 PM', '0:65') => [
+    0,  // Hour after treatment
+    5,  // Minute after treatment
+    1,  // Number of crossing the date
+    0,  // Number of switches between AM and PM
+  ] => '12:05 PM (next day)'
+
+(ex)
+  add_time('11:00 PM', '0:65', 'Monday') => [
+    7,  // Hour after treatment
+    5,  // Minute after treatment
+    1,  // Number of crossing the date
+    2,  // Number of switches between AM and PM
+    2,  // Sunday: 0, ... Saturday: 6
+  ] => '12:05 PM, Tuesday (next day)'
 '''
 
 def add_time(start, duration, *day):
@@ -131,20 +159,114 @@ def add_time(start, duration, *day):
   Extract time from parameter
   '''
   
-  start_HH = start.split()[0].split(':')[0]
-  start_MM = start.split()[0].split(':')[1]
-  start_STR = start.split()[1]
-  duration_HH = duration.split(':')[0]
-  duration_MM = duration.split(':')[1]
-  day_of_week = day[0] if len(day) > 0 else ''
+  start_HH = int(start.split()[0].split(':')[0])
+  start_MM = int(start.split()[0].split(':')[1])
+  start_Notation = start.split()[1]
+  duration_HH = int(duration.split(':')[0])
+  duration_MM = int(duration.split(':')[1])
+  if len(day) > 0:
+    if day[0].upper() == 'SUNDAY':
+      day_of_week = 0
+    if day[0].upper() == 'MONDAY':
+      day_of_week = 1
+    if day[0].upper() == 'TUESDAY':
+      day_of_week = 2
+    if day[0].upper() == 'WEDNESDAY':
+      day_of_week = 3
+    if day[0].upper() == 'THURSDAY':
+      day_of_week = 4
+    if day[0].upper() == 'FRIDAY':
+      day_of_week = 5
+    if day[0].upper() == 'SATURDAY':
+      day_of_week = 6
   
   '''
-  Calculate Minutes
+  Minute after treatment
   '''
   
-  #print(add_HH(11, 'AM', 10)) # [9, 0, 1]
-  #print(add_HH(11, 'AM', 20)) # [7, 1, 2]
-  #print(add_HH(11, 'PM', 10)) # [9, 1, 1]
-  #print(add_HH(11, 'PM', 20)) # [7, 1, 2]
+  list_MM = add_MM(start_MM, duration_MM)
+  
+  '''
+  Hour after treatment
+  '''
+  
+  if (start_HH + duration_HH + list_MM[1]) % 12 == 0:
+    HH = 12
+  else:
+    HH = (start_HH + duration_HH + list_MM[1]) % 12
+  
+  '''
+  Number of crossing the date
+  '''
+  
+  crossing = 0
+  for val in range(set_HH(start_HH, start_Notation), set_HH(start_HH, start_Notation) + duration_HH + list_MM[1] + 1):
+    if val % 24 == 0:
+      crossing += 1
+  
+  '''
+  Number of switches between AM and PM
+  '''
+  
+  switches = 0
+  for val in range(set_HH(start_HH, start_Notation), set_HH(start_HH, start_Notation) + duration_HH + list_MM[1] + 1):
+    if val % 12 == 0:
+      switches += 1
+  
+  '''
+  Preparing the return value (HH)
+  '''
+  
+  return_HH = str(HH)
+  
+  '''
+  Preparing the return value (MM)
+  '''
+  
+  return_MM = str(list_MM[0])
+  if len(return_MM) == 1:
+    return_MM = return_MM.rjust(2, '0')
+  
+  '''
+  Preparing the return value (Notation)
+  '''
+  
+  return_Notation = start_Notation
+  if switches % 2 == 1:
+    return_Notation = set_Notation(start_Notation)
+  
+  '''
+  Preparing the return value (Weekday)
+  '''
+  
+  DAY = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  if len(day) > 0:
+    return_Weekday = DAY[(day_of_week + crossing) % 7]
+  
+  '''
+  Preparing the return value (Nextday)
+  '''
+  
+  return_Nextday = ''
+  if crossing == 1:
+    return_Nextday = '(next day)'
+  if crossing > 1:
+    return_Nextday = '(' + str(crossing) + ' days later)'
+  
+  '''
+  Preparing the return value (ALL)
+  '''
+
+  return_All = return_HH + ':' + return_MM + ' ' + return_Notation
+  if len(day) != 0:
+    return_All += ', ' + return_Weekday
+  if crossing != 0:
+    return_All += ' ' + return_Nextday
+  
+  new_time = return_All
+  
+  '''
+  Return the return value
+  '''
   
   return new_time
